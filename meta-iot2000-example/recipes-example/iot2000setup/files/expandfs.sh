b2b56bb1dfc352ebfdb2b58b96adf5a39b594162
Jan Kiszka <jan.kiszka@siemens.com>
2017-07-29 10:12:43 +0200

    example: Clean up expandfs script
    Simplify some logic and ensure that we can define the partition number
    only via PART_NUMBER.
    
    Also, there is no need to delete the script after use. Removing it from
    autostart is sufficient.
    
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- meta-iot2000-example/recipes-example/iot2000setup/files/expandfs.sh
+++ meta-iot2000-example/recipes-example/iot2000setup/files/expandfs.sh
@@ -2,25 +2,24 @@
 
 set -eu
 
-DEVICE_TYPE=$(mount | head -n1 | cut -f 1 -d ' ' | cut -f 3 -d / | head -c 1) # returns 's' for boot from USB, 'm' for boot from SD
+PART_NUMBER=2
 
-if [ $DEVICE_TYPE == "s" ]
-	then
-		ROOT_PARTITION="/dev/sda2"
-		ROOT_DEVICE="/dev/sda"
-		PART_NUMBER=2 
-	elif [ $DEVICE_TYPE=="m" ]
-		then
-			ROOT_PARTITION="/dev/mmcblk0p2"
-			ROOT_DEVICE="/dev/mmcblk0"
-			PART_NUMBER=2 
-	else
-		echo "Cannot determine boot device."
-		exit 1
-fi
+case $(mount | grep "on / ") in
+/dev/mmcblk0*)
+	ROOT_DEVICE="/dev/mmcblk0"
+	ROOT_PARTITION="/dev/mmcblk0p$PART_NUMBER"
+	;;
+/dev/sda*)
+	ROOT_DEVICE="/dev/sda"
+	ROOT_PARTITION="/dev/sda$PART_NUMBER"
+	;;
+*)
+	echo "Cannot determine boot device."
+	exit 1
+	;;
+esac
 
-
-START_BLOCK=$(parted $ROOT_DEVICE -ms unit s p | grep "^2" | cut -f 2 -d: | rev | cut -c 2- | rev)
+START_BLOCK=$(parted $ROOT_DEVICE -ms unit s p | grep "^$PART_NUMBER" | cut -f 2 -d: | sed "s/s$//")
 
 parted -ms $ROOT_DEVICE rm $PART_NUMBER
 
@@ -30,4 +29,3 @@ partprobe $ROOT_DEVICE
 resize2fs $ROOT_PARTITION
 
 update-rc.d -f expandfs.sh remove
-rm /etc/init.d/expandfs.sh
