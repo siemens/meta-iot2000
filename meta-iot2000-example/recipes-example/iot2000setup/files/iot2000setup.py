ede0f5fdd492bfe01f18aabd70be7c3a7a926121
Xingxing He <xingxing.he@siemens.com>
2018-02-05 16:13:27 +0100

    example: iot2000setup: Enhance with cellular modem configuration
    This adds support for configuring cellular modems that are recognized by
    connman/ofono with the iot2000setup tool.
    
    The setup has been tested successfully with Huawei USB modem MS2131i-8,
    USB ID 12d1:1506.
    
    Signed-off-by: Xingxing He <xingxing.he@siemens.com>
    [Jan: python3 port, typo fixes]
    Signed-off-by: Jan Kiszka <jan.kiszka@siemens.com>

--- meta-iot2000-example/recipes-example/iot2000setup/files/iot2000setup.py
+++ meta-iot2000-example/recipes-example/iot2000setup/files/iot2000setup.py
@@ -11,6 +11,8 @@ import os
 import os.path
 import stat
 import mraa
+import sys
+import time
 from shutil import copyfile
 
 sys.settrace
@@ -27,7 +29,6 @@ interfacesConfig = """# /etc/network/interfaces -- configuration file for ifup(8
 auto lo
 iface lo inet loopback
 
-# Wired interfaces
 """	
 dhcpTemplate = """auto [interfaceName]
 iface [interfaceName] inet dhcp
@@ -70,13 +71,18 @@ src/gz i586-nlp-32 http://[host]/ipk/i586-nlp-32
 src/gz i586-nlp-32-intel-common http://[host]/ipk/i586-nlp-32-intel-common
 src/gz iot2000 http://[host]/ipk/iot2000
 '''
+
+# Ofono Context Header
+apnHeaderStr = "        AccessPointName = "
+userHeaderStr = "        Username = "
+pwdHeaderStr = "        Password = "
 	
 class TopMenu:
 	def __init__(self):
 		
 		# Use dmidecode to determine device type (IOT2040/IOT2000)
 		task = subprocess.Popen("/usr/sbin/dmidecode -t 11 | awk 'NR==8' | cut -f 2 -d :", stdout=subprocess.PIPE, shell=True)
-		device = task.stdout.read().lstrip().rstrip()
+		device = task.stdout.read().decode().lstrip().rstrip()
 
 		self.deviceIsIot2020 = False
 		if (device == "IOT2020"):
@@ -143,74 +149,223 @@ class OsSettings:
 		subprocess.call("passwd")
 		self.finish = True
 
+def readFileLines(fileName, startLine):
+	fileReading = open(fileName, 'r')
+	return "".join(fileReading.readlines()[startLine:])
+
 class Networking:
 	def __init__(self, topmenu):
 		self.topmenu = topmenu
 		self.finish = False
-		self.eth1present = not topmenu.deviceIsIot2020
-		self.wifiPresent = os.path.isdir("/sys/class/net/wlan0")
+		self.eth0HWPresent = True
+		self.eth1HWPresent = not topmenu.deviceIsIot2020
+		self.wifiHWPresent = os.path.isdir("/sys/class/net/wlan0")
+		self.networkInterfaces = {'eth0': '', 'eth1': '', 'wlan0': ''}
+		self.cellularInterface = []
+		self.cellularSimInfo = {}
 	def show(self):
-		menuItems = [("Configure Interfaces", self.configureInterfaces)]
-		if(self.wifiPresent):
-			menuItems.append(("Connect Wifi", self.configureWifi))
-		while(True and not self.finish):
-			action, selection = ListboxChoiceWindow(
-				self.topmenu.gscreen, 
-				"Networking", "",
-				menuItems,
-				[('Back', 'back', 'ESC')])
-			if(action == 'back'):
-				return
-			selection()
-	def configureInterfaces(self):
-		interfaces = ["eth0"]
-		if(self.eth1present):
-			interfaces.append("eth1")
-		if(self.wifiPresent):
-			interfaces.append("wlan0")
-		settings = []
-		for entry in interfaces:
-			settings.append((entry, self.getInterfaceConfig(entry)))
+		self.reconfigWifi = False
+		self.interfaceFileChange = False
+		self.newSettingsFile = interfacesConfig
+
+		# Get Get current configurations of each interface
+		self.getCurrentInterfaces(self.networkInterfaces)
+		self.cellularInterface = iot2000Connman.getCellularTechnology()
+		self.cellularSimInfo = iot2000Connman.getOfonoSimManager()
+
+		# Show check box
+		bb = ButtonBar(self.topmenu.gscreen, (("Ok", "ok"), ("Cancel", "cancel", "ESC")))
+		ct = CheckboxTree(height=7, scroll=1, width=40)
+		self.showCheckbox(ct)
+
+		gridFormText = "Cross [*] to select active interfaces"
+		g = GridForm(self.topmenu.gscreen, gridFormText, 1, 4)
+		g.add(ct, 0, 1)
+		g.add(bb, 0, 3, growx=1)
+		result = g.runOnce()
+
+		if (bb.buttonPressed(result) != "ok"):
+			return;
+
+		# Get user selection
+		selectedOptions = ct.getSelection()
+		eth0Enabled = "eth0" in selectedOptions
+		eth1Enabled = "eth1" in selectedOptions
+		wifiEnabled = "Wifi" in selectedOptions
+		cellularEnabled = "Cellular" in selectedOptions
+
+		# Show sub-config
+		if eth0Enabled:
+			self.showEthConfig("eth0")
+		if eth1Enabled:
+			self.showEthConfig("eth1")
+		if wifiEnabled:
+			self.showWifiConfig("wlan0")
+		if cellularEnabled:
+			self.showCellularConfig()
+
+		# Show new config
+		wpa_conf = readFileLines("/etc/wpa_supplicant.conf", 4)
+		self.cellularInterface = iot2000Connman.getCellularTechnology()
+		infoText = "Interface config:" + "\n" \
+				   + self.newSettingsFile \
+				   + "wlan0 config: " + "\n" \
+				   + "".join(wpa_conf) + "\n" \
+				   + "cellular: " + str(cellularEnabled) + "\n" + "\n".join(self.cellularInterface)
+		ret = ButtonChoiceWindow(
+			self.topmenu.gscreen,
+			"Press OK to enable New Config",
+			infoText,
+			[("OK", "ok"), ("Cancel", "cancel", "ESC")],
+			50, 50,
+			None)
+		if (ret != "ok"):
+			return
+
+		# down unselected interfaces
+		if not eth0Enabled:
+			subprocess.call("/sbin/ifdown eth0", shell=True, stderr=open(os.devnull, 'wb'))
+		if not eth1Enabled:
+			subprocess.call("/sbin/ifdown eth1", shell=True, stderr=open(os.devnull, 'wb'))
+		if not wifiEnabled:
+			subprocess.call("/sbin/ifdown wlan0", shell=True, stderr=open(os.devnull, 'wb'))
+		if not cellularEnabled:
+			iot2000Connman.disconnectCellular()
+
+
+		# Write interface file
+		fileName = "/etc/network/interfaces"
+		backupFileName = "/etc/network/interfaces.bak"
+		copyfile(fileName, backupFileName)
+		interfacesFile = open(fileName, 'w')
+		interfacesFile.write(self.newSettingsFile)
+		interfacesFile.close()
+
+		# restartNetwork?
+		newTempInterface = {'eth0': '', 'eth1': '', 'wlan0': ''}
+		self.getCurrentInterfaces(newTempInterface)
+		for key in self.networkInterfaces.keys():
+			if newTempInterface[key] != self.networkInterfaces[key]:
+				self.interfaceFileChange = True
+
+		if self.interfaceFileChange or self.reconfigWifi:
+			self.restartNetwork()
+
+	def showEthConfig(self, interface):
+		title = "Configure " + interface
+		settings = [(interface, self.networkInterfaces[interface])]
+		# ret is tuple type
 		ret = EntryWindow(
 			self.topmenu.gscreen,
-			"Configure Network Interfaces",
+			title,
 			"Specify IP addresses for network interfaces, enter 'dhcp' to obtain address by DHCP.",
 			settings,
 			1,
 			70, 50,
 			['OK', ('Cancel', 'cancel', 'ESC')],
 			None)
-			
-		if(ret[0] == "cancel"):
-			return;
 
-		newValues = ret[1]
-		settingsChanged = False
-		newSettingsFile = interfacesConfig
-		
-		for i in range(len(settings)):
-			if(newValues[i] != settings[i][1]):
-				settingsChanged = True
-			newSettingsFile += self.getConfigString(settings[i][0], newValues[i])
-		if(not settingsChanged):
+		if (ret[0] == "cancel"):
 			return
-		
-		fileName = "/etc/network/interfaces"
-		backupFileName = "/etc/network/interfaces.bak"
+
+		# Update eth config
+		self.updateNewInterfaceConfig(interface, ret[1][0].rstrip(), settings[0][1])
+
+	def showWifiConfig(self, interface):
+		title = "Configure " + interface
+		settings = [(interface, self.networkInterfaces[interface]), ("Type:", "WPA-PSK"), ("SSID:", ""), ("Key:", "")]
+		ret = EntryWindow(
+			self.topmenu.gscreen,
+			title,
+			"Specify IP addresses for network interfaces, enter 'dhcp' to obtain address by DHCP.",
+			settings,
+			1,
+			70, 50,
+			['OK', ('Cancel', 'cancel', 'ESC')],
+			None)
+
+		if (ret[0] == "cancel"):
+			return
+
+		# Update wifi interace
+		self.updateNewInterfaceConfig(interface, ret[1][0].rstrip(), settings[0][1])
+
+		# Update wpa config
+		for i in range(1, len(settings)):
+			if (ret[1][i] != settings[i][1]):
+				self.reconfigWifi = True
+				break
+
+		wpaConfig = wpaFileTemplate.replace("[type]", ret[1][1].rstrip()).replace("[ssid]", ret[1][2].rstrip()).replace(
+			"[passwd]", ret[1][3].rstrip())
+
+		fileName = "/etc/wpa_supplicant.conf"
+		backupFileName = "/etc/wpa_supplicant.conf.bak"
 		copyfile(fileName, backupFileName)
-		interfacesFile = open(fileName, 'w')
-		interfacesFile.write(newSettingsFile)
-		interfacesFile.close()
-		
-		self.restartNetwork()
+
+		wpaFile = open(fileName, 'w')
+		wpaFile.write(wpaConfig)
+		wpaFile.close()
+
+	def showCellularConfig(self):
+		settings = []
+
+		reconfigAPN = False
+		apnDict = iot2000Connman.getOfonoAPN()
+		settings.append(("APN:", apnDict[apnHeaderStr]))
+		settings.append(("Username:", apnDict[userHeaderStr]))
+		settings.append(("Password:", apnDict[pwdHeaderStr]))
+
+		pinStr= self.cellularSimInfo['PinRequired']
+		if pinStr == "puk":
+			settings.append(("Puk:", ""))
+			reconfigPin = True
+		elif pinStr == "pin":
+			settings.append(("Pin:", ""))
+			reconfigPin = True
+		elif pinStr == "none":
+			reconfigPin = False
+
+		#
+		title = "Configure Cellular"
+		infoText = "SIM Setting for modem " + self.cellularSimInfo['ModemPath']
+		ret = EntryWindow(
+			self.topmenu.gscreen,
+			title,
+			infoText,
+			settings,
+			1,
+			70, 50,
+			['OK', ('Cancel', 'cancel', 'ESC')],
+			None)
+
+		if (ret[0] == "cancel"):
+			return
+
+		# Update APN config?
+		for i in range(len(apnDict)):
+			if (ret[1][i] != settings[i][1]):
+				reconfigAPN = True
+				break
+
+		# Flow: disconnect---set params---connect
+		if reconfigAPN or reconfigPin:
+			iot2000Connman.disconnectCellular()
+
+		if reconfigPin:
+			iot2000Connman.configPin(ret[1][3].rstrip(), pinStr, self.cellularSimInfo['ModemPath'])
+			time.sleep(1)
+			self.cellularInterface = iot2000Connman.getCellularTechnology()
+
+		iot2000Connman.connectCellular(ret[1][0].rstrip(), ret[1][1].rstrip(), ret[1][2].rstrip())
+
 	def getInterfaceConfig(self, interface):
 		lines = [line.rstrip('\n') for line in open('/etc/network/interfaces')]
-		for lineNumber in range(0, len(lines)-1):
-			searchString = "auto " + interface
-
+		searchString = "auto " + interface
+		for lineNumber in range(0, len(lines) - 1):
 			if (searchString in lines[lineNumber]):
 				splitLine = lines[lineNumber].split()
-				
+
 				while (splitLine[0] != "iface"):
 					lineNumber += 1
 					splitLine = lines[lineNumber].split()
@@ -223,53 +378,64 @@ class Networking:
 						splitLine = lines[lineNumber].split()
 					return splitLine[1]
 			lineNumber += 1
-		return ""	
+		return ""
+
 	def getConfigString(self, interface, config):
-		if("wlan0" == interface):
-			if("dhcp" == config):
+		if ("wlan0" == interface):
+			if ("dhcp" == config):
 				return wirelessDhcpTemplate.replace("[interfaceName]", interface)
 			else:
 				return wirelessStaticTemplate.replace("[interfaceName]", interface).replace("[ip]", config)
-		if("dhcp" == config):
+
+		if ("dhcp" == config):
 			return dhcpTemplate.replace("[interfaceName]", interface)
 		else:
 			return staticTemplate.replace("[interfaceName]", interface).replace("[ip]", config)
+
 	def restartNetwork(self):
 		self.topmenu.gscreen.finish()
-		print(ansicolors.clear) # Clear console
+		print(ansicolors.clear)  # Clear console
 		print(ansicolors.reset)
 		subprocess.call("/etc/init.d/networking restart", shell=True)
-		
-		if(self.wifiPresent):
+
+		if (self.wifiHWPresent):
 			subprocess.call("/sbin/ifdown wlan0", shell=True)
 			subprocess.call("/sbin/ifup wlan0", shell=True)
-		
+
 		self.finish = True
-	def configureWifi(self):
-		ret = EntryWindow(
-			self.topmenu.gscreen,
-			"Configure WLAN",
-			"",
-			[("Type:", "WPA-PSK"), ("SSID:", ""), ("Key:", "")],
-			1,
-			70, 50,
-			['OK', ('Cancel', 'cancel', 'ESC')],
-			None)
-		if(ret[0] != "ok"):
-			return
-		
-		wpaConfig = wpaFileTemplate.replace("[type]", ret[1][0].rstrip()).replace("[ssid]", ret[1][1].rstrip()).replace("[passwd]", ret[1][2].rstrip())
-		
-		fileName = "/etc/wpa_supplicant.conf"
-		
-		backupFileName = "/etc/wpa_supplicant.conf.bak"
-		copyfile(fileName, backupFileName)
-		
-		wpaFile = open(fileName, 'w')
-		wpaFile.write(wpaConfig)
-		wpaFile.close()
-		
-		self.restartNetwork()
+
+	def getCurrentInterfaces(self, networkDict):
+		for key, value in networkDict.items():
+			networkDict[key] = self.getInterfaceConfig(key)
+
+	def showCheckbox(self, ct):
+		eth0CrossBox = self.eth0HWPresent and len(self.networkInterfaces['eth0'])
+		eth1CrossBox = self.eth1HWPresent and len(self.networkInterfaces['eth1'])
+		wifiCrossBox = self.wifiHWPresent and len(self.networkInterfaces['wlan0'])
+
+		cellularCrossBox = False
+		if len(self.cellularInterface):
+			if "  Connected = True" in self.cellularInterface:
+				cellularCrossBox = True
+
+		# Show checkbox in case that hardware is present
+		if self.eth0HWPresent:
+			ct.append("eth0", selected=eth0CrossBox)
+		if self.eth1HWPresent:
+			ct.append("eth1", selected=eth1CrossBox)
+		if self.wifiHWPresent:
+			ct.append("Wifi", selected=wifiCrossBox)
+		if 'ModemPath' in self.cellularSimInfo:
+			if len(self.cellularSimInfo['ModemPath']) > 0:
+				ct.append("Cellular", selected=cellularCrossBox)
+
+	def updateNewInterfaceConfig(self, interface, newConfig, oldConfig):
+		# Invalid newConfig if it is blank
+		if len(newConfig) > 0:
+			self.newSettingsFile += self.getConfigString(interface, newConfig)
+		elif len(oldConfig) > 0:
+			self.newSettingsFile += self.getConfigString(interface, oldConfig)
+
 
 class SoftwareSettings:
 	def __init__(self, topmenu):
@@ -632,9 +798,159 @@ class Peripherals:
 			elif selectedMode == "rs422":
 				return 2
 		return 0
+
+class connmanManager:
+	def __init__(self):
+		self.ethSupport = True
+		self.wifiSupport = True
+		self.cellularSupport = True
+		self.bluetoothSupport = False
+
+		self.connmanTechnologies = []
+		self.connmanServices = []
+		self.connmanSettings = ""
+		self.connmanConfigs = ""
+		self.cellularTechnology = []
+		self.cellularContext = ""
+		self.cellularCarrier = ""
+
+	def isOfonoRunning(self):
+		cellularOfono = False
+		task = subprocess.Popen("ps aux | grep -v grep | grep -i ofonod", stdout=subprocess.PIPE, shell=True)
+		if len(task.stdout.read().decode()) > 0:
+			cellularOfono = True
+		return cellularOfono
+
+	def getOfonoSimManager(self):
+		ofonoSimManager = {'Present': False, 'PinRequired': "none", 'LockedPins': False}
+
+		task = subprocess.Popen("/usr/lib/ofono/test/list-modems", stdout=subprocess.PIPE, shell=True)
+		ofonoModemInfo = task.stdout.read().decode()
+
+		if len(ofonoModemInfo) > 0:
+			if ofonoModemInfo.find("Present = 1") >= 0:
+				ofonoSimManager['Present'] = True
+			if ofonoModemInfo.find("LockedPins = pin") >= 0:
+				ofonoSimManager['LockedPins'] = True
+			if ofonoModemInfo.find("PinRequired = pin") >= 0:
+				ofonoSimManager['PinRequired'] = "pin"
+			elif ofonoModemInfo.find("PinRequired = puk") >= 0:
+				ofonoSimManager['PinRequired'] = "puk"
+			# Add dynamic ModemPath
+			modemPathStart = ofonoModemInfo.index('[')
+			modemPathEnd = ofonoModemInfo.index(']')
+			modemPathStr = ofonoModemInfo[modemPathStart+2:modemPathEnd-1]
+			ofonoSimManager['ModemPath'] = modemPathStr
+		return ofonoSimManager
+
+	def getOfonoAPN(self):
+		apnDict = {apnHeaderStr:"", userHeaderStr:"", pwdHeaderStr:""}
+		task = subprocess.Popen("/usr/lib/ofono/test/list-contexts", stdout=subprocess.PIPE, shell=True)
+		ofonoContextList = task.stdout.read().decode().split('\n')
+		for key in apnDict.keys():
+			for i in range(len(ofonoContextList)):
+				if ofonoContextList[i].find(key) >= 0:
+					apnDict[key] = ofonoContextList[i][len(key):]
+					break
+		return apnDict
+
+	def getConnmanTechnologies(self):
+		self.connmanTechnologies = []
+		self.cellularTechnology = []
+		if self.isOfonoRunning():
+			task = subprocess.Popen("/usr/bin/connmanctl technologies", stdout=subprocess.PIPE, shell=True)
+			self.connmanTechnologies = task.stdout.read().decode().split('\n')
+			for i in range(len(self.connmanTechnologies)):
+				if self.connmanTechnologies[i].find('cellular') >= 0:
+					self.cellularTechnology = self.connmanTechnologies[i: i + 6]
+					break
+
+	def getCellularTechnology(self):
+		self.getConnmanTechnologies()
+		return self.cellularTechnology
+
+	def getCellularContext(self):
+		self.cellularContext = ""
+		self.cellularCarrier = ""
+		task = subprocess.Popen("/usr/bin/connmanctl services", stdout=subprocess.PIPE, shell=True)
+		self.connmanServices = task.stdout.read().decode().split()
+		for i in range(len(self.connmanServices)):  # find the first cellular context
+			if self.connmanServices[i].find('cellular') >= 0:
+				self.cellularContext = self.connmanServices[i]
+				self.cellularCarrier = self.connmanServices[i - 1]
+				break
+
+	def getConnmanSettings(self):
+		file = open("/var/lib/connman/settings")
+		self.connmanSettings = file.read()
+		file.close()
+
+	def connectCellular(self, apn, user, pwd):
+		if not self.isOfonoRunning():
+			print("ofono is not running")
+		else:
+			if len(self.cellularTechnology) > 0:
+				# if modem need poweron
+				if "  Powered = False" in self.cellularTechnology:
+					self.powerOnCellular()
+
+				# connect Cellular
+				self.connectCellularContext()
+
+				# real connected?
+				if "  Connected = False" in self.getCellularTechnology():
+					self.initCellular(apn, user, pwd)
+			else:
+				print("--No cellular technology!!!---")
+
+	def initCellular(self, apn, user, pwd):
+		print("The Cellular need initialization")
+		internetContext = apn+" "+user+" "+pwd
+		subprocess.call("/usr/lib/ofono/test/create-internet-context " + internetContext, shell=True,
+						stderr=open(os.devnull, 'wb'))
+		subprocess.call("/usr/lib/ofono/test/online-modem", shell=True, stderr=open(os.devnull, 'wb'))
+		time.sleep(3)
+
+		# connect Cellular
+		self.connectCellularContext()
+
+		# real connected?
+		if "  Connected = False" in self.getCellularTechnology():
+			print("Cellular connection is failed")
+
+	def connectCellularContext(self):
+		# get cellular service and context
+		self.getCellularContext()
+		# Connect cellularlo
+		if len(self.cellularContext) > 0:
+			subprocess.call("/usr/bin/connmanctl connect " + self.cellularContext, shell=True, stderr=open(os.devnull, 'wb'))
+
+	def powerOnCellular(self):
+		subprocess.call("/usr/bin/connmanctl enable cellular", shell=True, stderr=open(os.devnull, 'wb'))
+		print("Cellular is in poweron, need to wait")
+		time.sleep(5)
+
+	def disconnectCellular(self):
+		# get cellular service
+		self.getCellularContext()
+		subprocess.call("/usr/bin/connmanctl disconnect " + self.cellularContext, shell=True,
+						stderr=open(os.devnull, 'wb'))
+
+	def configPin(self, pinCode, pinStr, path):
+		pinArgs = path + " " + pinStr + " " + pinCode
+		subprocess.call("/usr/lib/ofono/test/enter-pin "+pinArgs, shell=True,
+						stderr=open(os.devnull, 'wb'))
+		subprocess.call("/usr/lib/ofono/test/unlock-pin "+pinArgs, shell=True,
+						stderr=open(os.devnull, 'wb'))
+
+
+
+
+
+iot2000Connman = connmanManager()
+
 #repoFileTemplate
 mainwindow = TopMenu()
 while(True):
 	mainwindow.show()
-
 
